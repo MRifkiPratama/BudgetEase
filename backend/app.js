ae91c877826369aedafe33f5597d96f82c41f49d
Magnolia124 <muhammadrifkipratama@gmail.com>
2024-12-13 10:16:33 +0700

    deploy

--- backend/app.js
+++ backend/app.js
@@ -10,35 +10,38 @@ const donationRoutes = require('./routes/DonationRoute');
 const dotenv = require('dotenv');
 const app = express();
 const PORT = 5000;
+
+// CORS configuration
+app.use(cors({
+  origin: "https://budget-ease-frontened.vercel.app", // Update without trailing slash
+  methods: ["POST", "GET", "PUT", "DELETE"], // Include the HTTP methods you want to allow
+  allowedHeaders: ["Content-Type", "Authorization"], // Add any other headers you might need
+  credentials: true,  // Allow credentials (cookies, authorization headers)
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect('mongodb+srv://muhammadrifkipratama:rpl@budgetease.l8x08.mongodb.net/BudgetEase');
-app.use(cors(
-  {
-    origin: ["https://budget-ease-frontened.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
-app.use(express.urlencoded({extended: true}))
-app.use(bodyParser.json());
 
+// MongoDB connection
+mongoose.connect('mongodb+srv://muhammadrifkipratama:rpl@budgetease.l8x08.mongodb.net/BudgetEase');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function(){
-console.log('Connected to MongoDB Muhammad Rifki Pratama');
+  console.log('Connected to MongoDB');
 });
 
+// Test route to check server connection
 app.get('/', (req, res) => {
-  res.send('Backend connect test');
+  res.send('Backend connected successfully');
 });
 
+// Routes
 app.use('/users', userRoutes);
 app.use('/transaction', transactionRoutes);
 app.use('/report', reportRoutes);
-app.use('/donation', donationRoutes)
+app.use('/donation', donationRoutes);
 
-
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
